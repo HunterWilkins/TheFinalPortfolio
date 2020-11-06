9a5fcfe169e4f1f7370ebc3c8d747494f57b4b3f
HunterWilkins <wilkins.hunter@gmail.com>
2020-11-05 18:47:32 -0600

    Further styled the About page. Close to completion: scrollbar still has shadow on it, text slightly too hard to read

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -24,31 +24,34 @@ function About(props) {
             </div>
      
             <br></br>
-            <h2>Current Projects...</h2>
-            <br />
-            <ul className = "width-constraint">
-                <li>
-                    <h3>Spacepocalypse (Working Title)</h3>
-                    <br></br>
-                    <p className = "description">
-                        In the distant future, Earth has been destroyed by something or other, it doesn't matter. 
-                        What matters is that, in the fallout, one man is going to dedicate his life to saving 
-                        his hometown from destruction. He's going to accomplish this by doing everyone's chores.
-                        But they'll be <em>fun</em> chores.
-                    </p>
-                </li>
-                <li>
-                    <h3>The Slaying of the Malrog (Working Title)</h3>                    
-                    <br></br>
-                    <p className = "description">
-                        Every fantasy story has a prophecy or two floating about. What would happen, however, if a 
-                        destined hero of fate noticed something fishy about his role? Destined to slay the evil Malrog, 
-                        our hero arrives on the battlefield and sees only a harmless young woman. Sure, she attacks him, but 
-                        she doesn't <em>look</em> like a monster. How will he get out of this crazy (and, dare I say, <em>zany</em>)
-                        situation?
-                    </p>
-                </li>
-            </ul>
+            <div id = "current-projects">
+                <h1>Current Projects...</h1>
+               <br></br>
+                <ul className = "banner">
+                    <li>
+                        <h3><strong>Spacepocalypse</strong> (Working Title) <em>- Video Game</em></h3>
+                        <br></br>
+                        <p className = "description">
+                            In the distant future, Earth has been destroyed by something or other, it doesn't matter. 
+                            What matters is that, in the fallout, one man is going to dedicate his life to saving 
+                            his hometown from destruction. He's going to accomplish this by doing everyone's chores.
+                            But they'll be <em>fun</em> chores.
+                        </p>
+                    </li>
+                    <li>
+                        <h3><strong>The Slaying of the Malrog</strong> (Working Title) <em>- Book</em></h3>                    
+                        <br></br>
+                        <p className = "description">
+                            Every fantasy story has a prophecy or two floating about. What would happen, however, if a 
+                            destined hero of fate noticed something fishy about his role? Destined to slay the evil Malrog, 
+                            our hero arrives on the battlefield and sees only a harmless young woman. Sure, she attacks him, but 
+                            she doesn't <em>look</em> like a monster. How will he get out of this crazy (and, dare I say, <em>zany</em>)
+                            situation?
+                        </p>
+                    </li>
+                </ul>
+            </div>
+            
         </div>
     )
 }
