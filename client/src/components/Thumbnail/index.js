40b3a43f84fb790dd28f0a92d3c5ac913fc20548
HunterWilkins <wilkins.hunter@gmail.com>
2021-01-08 14:18:58 -0600

    Added custom loading gif (and subsequent CSS tweaks to accomodate it)

--- client/src/components/Thumbnail/index.js
+++ client/src/components/Thumbnail/index.js
@@ -22,7 +22,7 @@ function Thumbnail({thumbnail, genre, item, setFullImg, setFullscreen}) {
                 genre: item.genre
             });
             setFullscreen(true);
-            }} className = "thumbnail-box" key = {Math.random() * item.thumbnails.length} style = {imgLoaded ? {} : {backgroundImage: "url('/images/icons/loading.gif')"}}>
+            }} className = "thumbnail-box" key = {Math.random() * item.thumbnails.length} style = {imgLoaded ? {} : {backgroundImage: "url('/images/icons/loading.gif')", backgroundSize: "100%"}}>
                 <p className = "thumbnail-title" key = {Math.random() * item.thumbnails.length + thumbnail}>{fixTitle(thumbnail)}</p>
                 <img onLoad = {() => {setImgLoaded(true)}} onError = {() => setImgLoaded(true)} className = "thumbnail" src = {"/images/Artwork/" + item.genre + "/thumbnails/" + thumbnail} alt = {fixTitle(thumbnail)}></img>
         </figure>
